7bbd4c3049d5335337dae452d8690bfc23ba2fde
lbladma <tawmac@taws-Air.lan>
2021-11-17 06:31:48 -0500

    updated files

---
+++ client/build/precache-manifest.2020a8a26cb62c5d4aa6dfeb51fb2046.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1cb277de3e57569333eefa90b16bbab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a25c2a0f778e1cfc00d1",
+    "url": "/static/css/main.6816d842.chunk.css"
+  },
+  {
+    "revision": "1cf7551f9e7f70b64cd5",
+    "url": "/static/js/2.adbd702a.chunk.js"
+  },
+  {
+    "revision": "f333c948d07dc08e5747a9b83a79e23a",
+    "url": "/static/js/2.adbd702a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a25c2a0f778e1cfc00d1",
+    "url": "/static/js/main.e27959c8.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
